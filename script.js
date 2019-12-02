29269116918df49479072289e865fcbe4ee2a50f
Oleksii Mosunov <alexmosunov@gmail.com>
2019-12-02 14:19:12 +0200

    Uploading updates

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // 20191129123742
 // http://api.giphy.com/v1/gifs/search?q=cats&api_key=O3iJIz4KNHqyrwfM88y7Abn9WA2607z0&limit=8
-//images.original.url
-//
+
 const numOfEl = 8;
 
 class DataFromAPI {
@@ -9,106 +8,135 @@ class DataFromAPI {
     this.query = query;
     this.limit = limit;
   }
-async getResults () {
-  try {
-    const res = await fetch(
-      `http://api.giphy.com/v1/gifs/search?q=${this.query}&api_key=O3iJIz4KNHqyrwfM88y7Abn9WA2607z0&limit=${this.limit}`
-    );
-    const result = await res.json();
-    //const data = result.data;
-    //console.log(result.data);
-    return result.data;
-  } catch (error) {
-    alert(error);
+  async getResults() {
+    try {
+      const res = await fetch(
+        `http://api.giphy.com/v1/gifs/search?q=${this.query}&api_key=O3iJIz4KNHqyrwfM88y7Abn9WA2607z0&limit=${this.limit}`
+      );
+      const result = await res.json();
+      //const data = result.data;
+      //console.log(result.data);
+      this.result = result.data;
+      return result.data;
+    } catch (error) {
+      alert(error + " 1");
+    }
   }
-}};
+}
 
 (async () => {
-  const catsData = new DataFromAPI('cats', 40);
+  const catsData = new DataFromAPI("cats", 40);
   try {
     const myData = await catsData.getResults();
     pasteElements(myData);
+    console.log(myData);
 
-    document.querySelector('.refresh__btn').addEventListener('click', e => {
+    document.querySelector(".refresh__btn").addEventListener("click", e => {
+      clearInput();
       clearReasults();
       pasteElements(myData);
     });
 
+    infoButton();
   } catch (error) {
-    alert(error);
+    alert(error + "2");
   }
 })();
 
+const controlSearch = async () => {
+  const query = getInput();
+  console.log(query);
+  if (query) {
+    const search = new DataFromAPI(query, 40);
+    try {
+      const newData = await search.getResults();
+      // console.log(newData);
+      clearInput();
+      clearReasults();
+      pasteElements(newData);
+      document.querySelector(".refresh__btn").addEventListener("click", e => {
+        clearReasults();
+        pasteElements(newData);
+      });
+      infoButton();
+    } catch (error) {
+      alert(error + "3");
+    }
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
+
+const getInput = () => document.querySelector(".search__field").value;
+
 function clearReasults() {
-  document.querySelector('.image-grid').innerHTML = "";
+  document.querySelector(".image-grid").innerHTML = "";
 }
 
-// getResults("cats", 40).then(data => {
-//   pasteElements(data);
-//   // data.forEach(renderCard);
-//   // document.querySelector('.refresh__btn').addEventListener('click', pasteElements(data));
-// });
+const clearInput = () => {
+  document.querySelector(".search__field").value = "";
+};
 
-const pasteElements= data => {
+const pasteElements = data => {
   let randomNum;
   for (let i = 0; i < numOfEl; i++) {
     randomNum = Math.floor(Math.random() * 40);
     renderCard(data[randomNum]);
   }
-  // data.forEach(renderCard);
 };
-
-
-
-
-//console.log(catGifsEight)
-
-// catGifsEight.map(obj=>{
-//   return {
-//   gif: obj.images.original.url,
-//   jpg: obj.images[w_still].url
-//   }
-//   });
 
 function renderCard(obj) {
   const markup = `
-  <figure>
-  <img src="${obj.images["480w_still"].url}" class= "hidden jpg__js" alt="${obj.title}">
+  <figure class="gif-container">
+  <div class="info-image hidden">
+  <img src="${obj.images["480w_still"].url}" class= "jpg__js" alt="${
+    obj.title
+  }">
+  <span class="img-resolution">${obj.images["480w_still"].height}x${
+    obj.images["480w_still"].width
+  }</span>
+  <button class="btn btn-source"><a href="${
+    obj.url
+  }" target="_blank">Оригинал</a></button>
+  </div>
   <img src="${obj.images.original.url}" class="gif__js" alt="${obj.title}">
   <figcaption>
-      <span class='caption'>${obj.title}</span>
+      <span class='caption'>${limitTitle(obj.title)}</span>
       <button class="info-icon"><i>i</i></button>
   </figcaption>
 </figure>
 `;
-  document.querySelector('.image-grid').insertAdjacentHTML("afterbegin", markup);
-  // document.querySelector(".jpg__js").addEventListener("mouseover", () => {
-  //   document.querySelector(".gif__js").classList.remove("hidden");
-  //   document.querySelector(".jpg__js").classList.add("hidden");
-  // });
-  
-  // document.querySelector(".gif__js").addEventListener("mouseleave", () => {
-  //   document.querySelector(".gif__js").classList.add("hidden");
-  //   document.querySelector(".jpg__js").classList.remove("hidden");
-  // });
+  document
+    .querySelector(".image-grid")
+    .insertAdjacentHTML("afterbegin", markup);
+}
+function infoButton() {
+  const gifContainers = document.querySelectorAll(".gif-container");
+  gifContainers.forEach(el => {
+    el.querySelector(".info-icon").addEventListener("click", e => {
+      e.preventDefault();
+      el.querySelector(".jpg__js").classList.toggle("tinted");
+      el.querySelector(".gif__js").classList.toggle("hidden");
+      el.querySelector(".info-image").classList.toggle("hidden");
+    });
+  });
 }
 
-// const renderResults = data => {
-//       data.forEach(renderCard);
-// };
+const limitTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
 
-//renderCard(catGifsEight);
-
-// const renderResults = data => {
-//     data.forEach(renderAd);
-//   };
-
-// document.querySelector(".jpg__js").addEventListener("mouseover", () => {
-//   document.querySelector(".gif__js").classList.remove("hidden");
-//   document.querySelector(".jpg__js").classList.add("hidden");
-// });
-
-// document.querySelector(".gif__js").addEventListener("mouseleave", () => {
-//   document.querySelector(".gif__js").classList.add("hidden");
-//   document.querySelector(".jpg__js").classList.remove("hidden");
-// });
+    // Return the result
+    return `${newTitle.join(" ")} ...`;
+  }
+  return title;
+};
